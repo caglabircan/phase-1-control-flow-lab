c557f6c7c5348184071d287f43952c0c6e041c54
unknown <caglabircan@hotmail.com>
2022-10-10 21:52:33 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+
+//1st test
+
+function scuberGreetingForFeet(n){
+  if (n <= 400) {
+    return "This one is on me!";
+  } else if (n > 400 && n <= 2000) {
+    return "That will be twenty bucks.";
+  } else if (n >= 2000 && n <=2500) {
+    return 'I will gladly take your thirty bucks.';
+  } else {
+    return "No can do.";
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+//2nd test
+
+function ternaryCheckCity(city){
+  return ((city === "NYC") ? "Ok, sounds good." : "No go.");
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+ function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  } 
 }
+
+/* function switchOnCharmFromTip(tip) {
+  // Write your code here!
+  console.log(tip);
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+} */
